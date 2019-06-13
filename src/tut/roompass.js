cba95cf5e8c39d53f9f4ff2e08928ddb63aa890f
XHerna <morten.b2606@gmail.com>
2019-06-13 13:45:44 +0200

    test

--- src/tut/roompass.js
+++ src/tut/roompass.js
@@ -34,7 +34,7 @@ room.onPlayerLeave = () => {
 room.onRoomLink = () => {
     roles = room.getPlugin(`sav/roles`);
     if (room.password === null) {
-//        password = roles.getConfig().roles.admin;
+        password = roles.getConfig().roles.admin;
         room.setPassword(password);
 
     }
